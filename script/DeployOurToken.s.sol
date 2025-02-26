c4902a75f4437c52979374b24e1e76ec63e12491
otis-tran <otis.tran2000@gmail.com>
2025-02-26 22:56:25 +0700

    Write test for OurToken

--- script/DeployOurToken.s.sol
+++ script/DeployOurToken.s.sol
@@ -6,7 +6,6 @@ import {Script} from "forge-std/Script.sol";
 import {OurToken} from "../src/OurToken.sol";
 
 contract DeployOurToken is Script {
-
     uint256 public constant INITIAL_SUPPLY = 1000 ether;
 
     function run() external returns (OurToken) {
